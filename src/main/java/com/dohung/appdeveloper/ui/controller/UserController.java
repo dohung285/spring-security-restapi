5076c0ae8806b21ee5939000f1c87630c95f1f61
hungdx <dohung12369874@gmail.com>
2021-04-30 20:29:10 +0700

    thêm method lấy ra username đăng nhập

--- src/main/java/com/dohung/appdeveloper/ui/controller/UserController.java
+++ src/main/java/com/dohung/appdeveloper/ui/controller/UserController.java
@@ -6,16 +6,10 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import com.dohung.appdeveloper.exceptions.UserServiceException;
 import com.dohung.appdeveloper.service.UserService;
@@ -29,79 +23,98 @@ import com.dohung.appdeveloper.ui.shared.dto.UserDto;
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
-			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
-		if (userDetails.getFirstName().isEmpty()) {
-			throw new NullPointerException("The Objec is null");
-		}
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}, produces = {
+            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
+        if (userDetails.getFirstName().isEmpty()) {
+            throw new NullPointerException("The Objec is null");
+        }
 
-		UserRest returnValue = new UserRest();
-		UserDto userDto = new UserDto();
+        UserRest returnValue = new UserRest();
+        UserDto userDto = new UserDto();
 
-		BeanUtils.copyProperties(userDetails, userDto);
-		System.out.println(userDto);
+        BeanUtils.copyProperties(userDetails, userDto);
+        System.out.println(userDto);
 
-		UserDto createUser = userService.createUser(userDto);
-		BeanUtils.copyProperties(createUser, returnValue);
+        UserDto createUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createUser, returnValue);
 
-		return returnValue;
-	}
+        return returnValue;
+    }
 
-	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public UserRest getUsers(@PathVariable String id) {
-		UserRest returnValue = new UserRest();
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest getUsers(@PathVariable String id) {
+        UserRest returnValue = new UserRest();
 
-		UserDto userDto = userService.getUserByUserId(id);
-		BeanUtils.copyProperties(userDto, returnValue);
-		return returnValue;
-	}
+        UserDto userDto = userService.getUserByUserId(id);
+        BeanUtils.copyProperties(userDto, returnValue);
+        return returnValue;
+    }
 
-	@PutMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public UserRest updateUsers(@PathVariable String id, @RequestBody UserDetailsRequestModel userDetail) {
+    @PutMapping(path = "/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest updateUsers(@PathVariable String id, @RequestBody UserDetailsRequestModel userDetail) {
 
-		UserRest returnValue = new UserRest();
-		UserDto userDto = new UserDto();
+        UserRest returnValue = new UserRest();
+        UserDto userDto = new UserDto();
 
-		BeanUtils.copyProperties(userDetail, userDto);
+        BeanUtils.copyProperties(userDetail, userDto);
 
-		UserDto updateUser = userService.updateUser(id, userDto);
-		BeanUtils.copyProperties(updateUser, returnValue);
+        UserDto updateUser = userService.updateUser(id, userDto);
+        BeanUtils.copyProperties(updateUser, returnValue);
 
-		return returnValue;
-	}
+        return returnValue;
+    }
 
-	@DeleteMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public OperationStatusModel deleteUsers(@PathVariable String id) {
-		OperationStatusModel returnValue = new OperationStatusModel();
-		returnValue.setOperationName(RequestOperationName.DELETE.name());
+    @DeleteMapping(path = "/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public OperationStatusModel deleteUsers(@PathVariable String id) {
+        OperationStatusModel returnValue = new OperationStatusModel();
+        returnValue.setOperationName(RequestOperationName.DELETE.name());
 
-		userService.deleteUser(id);
+        userService.deleteUser(id);
 
-		returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+        returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
 
-		return returnValue;
-	}
+        return returnValue;
+    }
 
-	@GetMapping()
-	public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
-			@RequestParam(value = "limit", defaultValue = "25") int limit) {
+    @GetMapping()
+    public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+                                   @RequestParam(value = "limit", defaultValue = "25") int limit) {
 
-		List<UserRest> returnVales = new ArrayList<UserRest>();
-		List<UserDto> users = userService.getUsers(page, limit);
+        List<UserRest> returnVales = new ArrayList<UserRest>();
+        List<UserDto> users = userService.getUsers(page, limit);
 
-		for (UserDto userDto : users) {
-			UserRest userRest = new UserRest();
-			BeanUtils.copyProperties(userDto, userRest);
-			returnVales.add(userRest);
+        for (UserDto userDto : users) {
+            UserRest userRest = new UserRest();
+            BeanUtils.copyProperties(userDto, userRest);
+            returnVales.add(userRest);
+        }
 
-		}
+        return returnVales;
 
-		return returnVales;
+    }
 
-	}
+
+    // API
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserName() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            final String currentPrincipalName = authentication.getName();
+            System.out.println("Authentication: " + authentication);
+            System.out.println("Principal: " + authentication.getPrincipal());
+            System.out.println("getDetails: " + authentication.getDetails());
+            System.out.println("getCredentials: " + authentication.getCredentials());
+            return currentPrincipalName;
+        }
+
+        return null;
+    }
+
 
 }
